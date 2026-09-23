f8916f24925a1eb0f9470fc8f9fb6b9ac6b4ff48
ezno02 <dev.ezno@gmail.com>
2026-09-22 22:33:32 -0300

    commit 22/09

--- src/app/getPokemons.tsx
+++ src/app/getPokemons.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchPokemons() {
   try {
-    const response = await axios.post('https://beta.pokeapi.co/graphql/v1beta', {
+    const response = await axios.post(`https://beta.pokeapi.co/graphql/v1beta`, {
       query: `
         query GetPokemons {
           pokemon_v2_pokemon(limit: 1000) {
